28361a2379e6506881648f78bcf0faad6c16ec69
lc <1033581609@qq.com>
2021-01-19 21:07:25 +0800

    add: 获取音视频信息

---
+++ https/public/device/client.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const audioSource = document.querySelector("select#audioSource");
+const audioOutput = document.querySelector("select#audioOutput");
+const videoSource = document.querySelector("select#videoSource");
+async function getRtc() {
+    await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+}
+if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+    throw new Error("enumerateDevice is not support!");
+} else {
+    getRtc();
+    navigator.mediaDevices.enumerateDevices().then(gotDevices).catch(handleError);
+}
+function gotDevices(deviceInfos) {
+    // 遍历数组
+    deviceInfos.forEach(deviceInfo => {
+        console.log(`kind: ${deviceInfo.kind};label: ${deviceInfo.label};
+            deviceId=${deviceInfo.deviceId};groupId: ${deviceInfo.groupId}`);
+        let option = document.createElement("option");
+        option.text = deviceInfo.label;
+        option.value = deviceInfo.deviceId;
+        if (deviceInfo.kind === "audioinput") {
+            audioSource.appendChild(option);
+        } else if (deviceInfo.kind === "audiooutput") {
+            audioOutput.appendChild(option);
+        } else if (deviceInfo.kind === "videoinput") {
+            videoSource.appendChild(option);
+        }
+    });
+}
+
+function handleError(err) {
+    console.log(err.name, err.message);
+}
